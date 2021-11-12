290503e2a545112b2dc13566fadd1fe5ebe4ff30
dirkmeyboom <92581164+dirkmeyboom@users.noreply.github.com>
2021-11-12 14:57:36 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,34 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
+let stop_reeeeechts = 0
+let stop_liiiiinks = 0
+let liiiiinks = 0
+let reeeeechts = 0
+let voooooruit = 0
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
+        radio.sendString("voooooruit")
+    } else {
+        if (input.pinIsPressed(TouchPin.P0)) {
+            radio.sendString("liiiiinks")
+        } else {
+            if (input.pinIsPressed(TouchPin.P1)) {
+                radio.sendString("reeeeechts")
+            } else {
+                if (input.buttonIsPressed(Button.A)) {
+                    radio.sendString("stop liiiiinks")
+                } else {
+                    if (input.buttonIsPressed(Button.B)) {
+                        radio.sendString("stop reeeeechts")
+                    }
+                }
+            }
+        }
+    }
 })
 basic.forever(function () {
+    stop_reeeeechts = stop_reeeeechts
+    stop_liiiiinks = stop_liiiiinks
+    liiiiinks = liiiiinks
+    reeeeechts = reeeeechts
+    voooooruit = voooooruit
     radio.setGroup(69)
 })
